d608e52d986654a94354e70aa9b3f2c090a755de
polonez-byte-112 <70174448+polonez-byte-112@users.noreply.github.com>
2021-01-10 20:18:50 +0100

    Memory 4x4 version

--- Memory/src/com/college/Game.java
+++ Memory/src/com/college/Game.java
@@ -7,8 +7,8 @@ import java.awt.event.*;
 
 public class Game extends JFrame {
 
-    public int SCREEN_WIDTH=800;
-    public int SCREEN_HEIGHT=800;
+    public int SCREEN_WIDTH=400;
+    public int SCREEN_HEIGHT=400;
     public int spacing=5;
     int total = 0;
 
@@ -19,11 +19,11 @@ public class Game extends JFrame {
 
     //tablice
 
-    public int[] allTiles = {1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18,19,19,20,20,21,21,22,22,23,23,24,24,25,25,26,26,27,27,28,28,29,29,30,30,31,31,32,32};
+    public int[] allTiles = {1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8};
     public int[][] tiles2D;
 
-    public boolean[][] uncovered= new boolean[8][8];
-    public boolean[][] visible = new boolean[8][8];
+    public boolean[][] uncovered= new boolean[4][4];
+    public boolean[][] visible = new boolean[4][4];
 
 
 
@@ -49,42 +49,12 @@ public class Game extends JFrame {
 
     public Color color1= new Color(255,206,0);
     public Color color2= new Color(176,11,30);
-    public Color color3= new Color(95,114,15);
+    public Color color3= new Color(226,226,226);
     public Color color4= new Color(0,109,225);
     public Color color5= new Color(255,76,148);
-    public Color color6= new Color(108,35,95);
-    public Color color7= new Color(64,224,208);
-    public Color color8= new Color(0,0,0);
-    public Color color9= new Color(255,255,255);
-    public Color color10= new Color(135,44,204);
-
-
-    public Color color11= new Color(212,115,50);
-    public Color color12= new Color(50,212,115);
-    public Color color13= new Color(115,50,212);
-    public Color color14= new Color(212,50,66);
-    public Color color15= new Color(127,30,39);
-    public Color color16= new Color(39,127,30);
-    public Color color17= new Color(131,120,127);
-    public Color color18= new Color(109,53,53);
-    public Color color19= new Color(106,76,76);
-    public Color color20= new Color(0,0,128);
-
-
-
-    public Color color21= new Color(0,0,255);
-    public Color color22= new Color(255,0,0);
-    public Color color23= new Color(0,255,0);
-    public Color color24= new Color(0,46,46);
-    public Color color25= new Color(145,30,180);
-    public Color color26= new Color(220,190,255);
-    public Color color27= new Color(240,50,230);
-    public Color color28= new Color(150,215,180);
-    public Color color29= new Color(250,190,212);
-    public Color color30= new Color(230,25,75);
-
-    public Color color31 = new Color(128,128,0);
-    public Color color32 = new Color(128,0,0);
+    public Color color6= new Color(122,37,134);
+    public Color color7= new Color(136,186,116);
+    public Color color8= new Color(116,136,186);
 
 
 
@@ -116,11 +86,11 @@ public class Game extends JFrame {
 
 
         shuffleArray();
-       tiles2D = createDiagonalArray(allTiles,8,8);
+       tiles2D = createDiagonalArray(allTiles,4,4);
 
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
                 uncovered[i][j]=false;
                 visible[i][j]=false;
             }
@@ -176,8 +146,8 @@ public class Game extends JFrame {
 
     public int inBoxX(){
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
 
                 if(mouseX>i*100+4 && mouseX<=i*100+100-2*spacing+4 && mouseY>j*100+30 && mouseY<=j*100+100-2*spacing+30){
                     return i;
@@ -190,8 +160,8 @@ public class Game extends JFrame {
     }
     public int inBoxY(){
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
 
                 if(mouseX>i*100+4 && mouseX<=i*100+100-2*spacing+4 && mouseY>j*100+30 && mouseY<=j*100+100-2*spacing+30){
                     return j;
@@ -204,8 +174,8 @@ public class Game extends JFrame {
 
     public int inBoxClickedX(){
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
 
                 if(mouseClickedX>i*100+4 && mouseClickedX<=i*100+100-2*spacing+4 && mouseClickedY>j*100+30 && mouseClickedY<=j*100+100-2*spacing+30){
                     return i;
@@ -217,8 +187,8 @@ public class Game extends JFrame {
     }
     public int inBoxClickedY(){
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
 
                 if(mouseClickedX>i*100+4 && mouseClickedX<=i*100+100-2*spacing+4 && mouseClickedY>j*100+30 && mouseClickedY<=j*100+100-2*spacing+30){
                     return j;
@@ -245,7 +215,7 @@ public class Game extends JFrame {
 
     public void resetter(){
         shuffleArray();
-        tiles2D = createDiagonalArray(allTiles,8,8);
+        tiles2D = createDiagonalArray(allTiles,4,4);
 
         total=0;
         currentEvenXClick=-1;
@@ -260,8 +230,8 @@ public class Game extends JFrame {
         oddClick=false;
         cardsToUncoverLeft=3;
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
                 visible[i][j]=false;
                 uncovered[i][j]=false;
             }
@@ -276,19 +246,11 @@ public class Game extends JFrame {
             g.setColor(Color.lightGray);
             g.fillRect(0,0, SCREEN_WIDTH,SCREEN_HEIGHT);
 
-            for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 8; j++) {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
                     g.setColor(Color.darkGray);
 
-
-
-
-
-
-
                     if(uncovered[i][j]==true && cardsToUncoverLeft!=0 ){
-
-
 
                             if(tiles2D[i][j]==1){
                                 g.setColor(color1);
@@ -315,92 +277,8 @@ public class Game extends JFrame {
                             if(tiles2D[i][j]==8){
                                 g.setColor(color8);
                             }
-                            if(tiles2D[i][j]==9){
-                                g.setColor(color9);
-                            }
-                            if(tiles2D[i][j]==10){
-                                g.setColor(color10);
-                            }
-
-                            if(tiles2D[i][j]==11){
-                                g.setColor(color11);
-                            }
-                            if(tiles2D[i][j]==12){
-                                g.setColor(color12);
-                            }
-                            if(tiles2D[i][j]==13){
-                                g.setColor(color13);
-                            }
-                            if(tiles2D[i][j]==14){
-                                g.setColor(color14);
-                            }
-                            if(tiles2D[i][j]==15){
-                                g.setColor(color15);
-                            }
-                            if(tiles2D[i][j]==16){
-                                g.setColor(color16);
-                            }
-                            if(tiles2D[i][j]==17){
-                                g.setColor(color17);
-                            }
-                            if(tiles2D[i][j]==18){
-                                g.setColor(color18);
-                            }
-                            if(tiles2D[i][j]==19){
-                                g.setColor(color19);
-                            }
-                            if(tiles2D[i][j]==20){
-                                g.setColor(color20);
-                            }
-
-                            if(tiles2D[i][j]==21){
-                                g.setColor(color21);
-                            }
-                            if(tiles2D[i][j]==22){
-                                g.setColor(color22);
-                            }
-                            if(tiles2D[i][j]==23){
-                                g.setColor(color23);
-                            }
-                            if(tiles2D[i][j]==24){
-                                g.setColor(color24);
-                            }
-                            if(tiles2D[i][j]==25){
-                                g.setColor(color25);
-                            }
-                            if(tiles2D[i][j]==26){
-                                g.setColor(color26);
-                            }
-                            if(tiles2D[i][j]==27){
-                                g.setColor(color27);
-                            }
-                            if(tiles2D[i][j]==28){
-                                g.setColor(color28);
-                            }
-                            if(tiles2D[i][j]==29){
-                                g.setColor(color29);
-                            }
-                            if(tiles2D[i][j]==30){
-                                g.setColor(color30);
-                            }
-
-                            if(tiles2D[i][j]==31){
-                                g.setColor(color31);
-                            }
-
-                            if(tiles2D[i][j]==32){
-                                g.setColor(color32);
-                            }
-
 
                     }
-
-
-
-
-
-
-
 
 
                     if(cardsToUncoverLeft==0 && visible[i][j]==false){
@@ -432,84 +310,8 @@ public class Game extends JFrame {
                         if(tiles2D[i][j]==7){
                             g.setColor(color7);
                         }
-                        if(tiles2D[i][j]==8){
+                        if(tiles2D[i][j]==8) {
                             g.setColor(color8);
-                        }
-                        if(tiles2D[i][j]==9){
-                            g.setColor(color9);
-                        }
-                        if(tiles2D[i][j]==10){
-                            g.setColor(color10);
-                        }
-
-                        if(tiles2D[i][j]==11){
-                            g.setColor(color11);
-                        }
-                        if(tiles2D[i][j]==12){
-                            g.setColor(color12);
-                        }
-                        if(tiles2D[i][j]==13){
-                            g.setColor(color13);
-                        }
-                        if(tiles2D[i][j]==14){
-                            g.setColor(color14);
-                        }
-                        if(tiles2D[i][j]==15){
-                            g.setColor(color15);
-                        }
-                        if(tiles2D[i][j]==16){
-                            g.setColor(color16);
-                        }
-                        if(tiles2D[i][j]==17){
-                            g.setColor(color17);
-                        }
-                        if(tiles2D[i][j]==18){
-                            g.setColor(color18);
-                        }
-                        if(tiles2D[i][j]==19){
-                            g.setColor(color19);
-                        }
-                        if(tiles2D[i][j]==20){
-                            g.setColor(color20);
-                        }
-
-                        if(tiles2D[i][j]==21){
-                            g.setColor(color21);
-                        }
-                        if(tiles2D[i][j]==22){
-                            g.setColor(color22);
-                        }
-                        if(tiles2D[i][j]==23){
-                            g.setColor(color23);
-                        }
-                        if(tiles2D[i][j]==24){
-                            g.setColor(color24);
-                        }
-                        if(tiles2D[i][j]==25){
-                            g.setColor(color25);
-                        }
-                        if(tiles2D[i][j]==26){
-                            g.setColor(color26);
-                        }
-                        if(tiles2D[i][j]==27){
-                            g.setColor(color27);
-                        }
-                        if(tiles2D[i][j]==28){
-                            g.setColor(color28);
-                        }
-                        if(tiles2D[i][j]==29){
-                            g.setColor(color29);
-                        }
-                        if(tiles2D[i][j]==30){
-                            g.setColor(color30);
-                        }
-
-                        if(tiles2D[i][j]==31){
-                            g.setColor(color31);
-                        }
-
-                        if(tiles2D[i][j]==32){
-                            g.setColor(color32);
                         }
                     }
                     g.fillRect(i*100, j*100, 100-2*spacing, 100-2*spacing);
@@ -548,7 +350,7 @@ public class Game extends JFrame {
             if(cardsToUncoverLeft<=1){
 
                 cardsToUncoverLeft=0;
-                for (int i = 1; i < 33; i++) {
+                for (int i = 1; i < 9; i++) {
                     if(tiles2D[currentOddXClick][currentOddYClick]!=i && tiles2D[currentEvenXClick][currentEvenYClick]!=i){
                         uncovered[currentOddXClick][currentOddYClick]=false;
                         uncovered[currentEvenXClick][currentEvenYClick]=false;
